0f2afad4ad0ac4b75716f645d363b53181a493ef
Anjali-292005 <anjali20054247@gmail.com>
2026-02-09 20:38:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Welcome t react with vite</h1>
+      <h2>Start the project</h2>
+    </div>
+    
+  )
+}
+
+export default App
